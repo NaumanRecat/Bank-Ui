50893efd2364468312a6f0dbc137822f8f9b5616
NaumanRecat <naumantariq2977@gmail.com>
2021-10-04 00:25:01 -0700

    si

---
+++ src/screen/Signup1.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, TextInput} from 'react-native';
+
+import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
+import DatePicker from 'react-native-datepicker';
+
+import {HeaderText} from '../component/HeaderText';
+import {ContentText} from '../component/ContentText';
+import { Input } from "../component/Input";
+import { Btn } from "../component/Btn";
+
+export class Signup1 extends React.Component{
+    render(){
+        return(
+            <View style={{
+                flex:1,
+            }}>
+                <HeaderText titles={'StatBanking'} />
+                <ContentText title={'Tell us about yourself'} />
+                <View
+                style={{
+                    paddingLeft: h('5%'),
+                }}>
+                    <Text>  I am a ..........</Text>
+                </View>
+                <View style={{
+                    alignItems:'center',
+                    justifyContent:'center',
+                    marginTop:h('4%'),
+                }}>
+                
+                <TextInput numberOfLines={10} placeholder={'Person'} style={{
+                     height:h('27%'),
+                     width: w('80%'),
+                     borderWidth: h('0.08%'),
+                     borderRadius: h('2%'),
+                     marginBottom: h('2%'),  
+                      }}/>
+
+                <TextInput  numberOfLines={10} placeholder={'Business'} style={{
+                     height:h('27%'),
+                     width: w('80%'),
+                     borderWidth: h('0.08%'),
+                     borderRadius: h('2%'),  
+                      }}/>
+                </View>
+                <View style={{
+                    marginTop:h('5%'),
+                    alignItems:'center',
+                    justifyContent:'center',
+                }}>
+                <Btn title={'Next'} />
+                </View>
+                
+            </View>
+        )
+    }
+}
