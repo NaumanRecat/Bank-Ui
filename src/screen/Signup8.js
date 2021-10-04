dd29a34cd5ae75051f35163630d802634e939292
NaumanRecat <naumantariq2977@gmail.com>
2021-10-04 07:00:23 -0700

    signup8

---
+++ src/screen/Signup8.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, TextInput} from 'react-native';
+
+import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
+import DatePicker from 'react-native-datepicker';
+
+import {HeaderText} from '../component/HeaderText';
+import {ContentText} from '../component/ContentText';
+import { Input } from "../component/Input";
+import { Btn } from "../component/Btn";
+
+export class Signup8  extends React.Component{
+    render(){
+        return(
+            <Text> Testing</Text>
+        )
+    }
+}
