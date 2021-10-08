d30a896667645c1fb2ac2d19007149247e456cf6
NaumanRecat <naumantariq2977@gmail.com>
2021-10-08 07:04:49 -0700

    done

--- src/screen/Signup7_5.js
+++ src/screen/Signup7_5.js
@@ -1,12 +1,9 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, TextInput} from 'react-native';
+import React from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
 
 import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
-import DatePicker from 'react-native-datepicker';
 
 import {HeaderText} from '../component/HeaderText';
-import {ContentText} from '../component/ContentText';
-import { Input } from "../component/Input";
 import { Btn } from "../component/Btn";
 
 export class Signup7_5 extends React.Component{
@@ -15,68 +12,56 @@ export class Signup7_5 extends React.Component{
         return(
             <View>
                 <HeaderText titles={'Choose a Plan'} />
-                <View style={{
-                    alignItems:'center',
-                    justifyContent:'center',
-                    marginTop: h('3%')
-                }}>
-                <TextInput placeholder={'Varcity Plan'} style={{
-                     height:h('17%'),
-                     width: w('85%'),
-                     borderWidth: h('0.08%'),
-                     borderRadius: h('2%'),  
-                      }}/>
-
-                      <View style={{
-                          height: h('2%'),
-                      }} />
-
-                    <TextInput placeholder={'Varcity Plan'} style={{
-                     height:h('17%'),
-                     width: w('85%'),
-                     borderWidth: h('0.08%'),
-                     borderRadius: h('2%'),  
-                      }}/>
-
-                    <View style={{
-                          height: h('2%'),
-                      }} />
-
-                    <TextInput placeholder={'Varcity Plan'} style={{
-                     height:h('16%'),
-                     width: w('85%'),
-                     borderWidth: h('0.08%'),
-                     borderRadius: h('2%'),  
-                      }}/>
-
-                    <View style={{
-                          height: h('2%'),
-                      }} />
-
-                      <View>
-                          <TextInput placeholder={'Promo Code (Optional)'} style={{
-                              height:h('8%'),
-                              width:w('80%'),
-                              borderWidth: h('0.08%'),
-                              borderRadius: h('1.3%'),  
-                              marginBottom: h('1%'),
-                          }}/>
-                          <TextInput placeholder={'Referral Code (Optional)'} style={{
-                              height:h('8%'),
-                              width:w('80%'),
-                              borderWidth: h('0.08%'),
-                              borderRadius: h('1.3%'),  
-                          }}/>
-                          
-                      </View>     
-                </View>
-                <View style={{
-                    marginTop: h('2%'),
-                    alignItems:'center',
-                }}>
-                    <Btn Button={() => this.props.navigation.navigate('Signup9')}  title={'Next'}  />
-                </View>
+            <View style={styles.inputtextcontainer}>
+                <TextInput placeholder={'Varcity Plan'} style={styles.input1}/>
+            <View style={styles.seprator} />
+                <TextInput placeholder={'Varcity Plan'} style={styles.input1}/>
+            <View style={styles.seprator} />
+                <TextInput placeholder={'Varcity Plan'} style={styles.input1}/>
+            <View style={styles.seprator} />
+            <View>
+                <TextInput placeholder={'Promo Code (Optional)'} style={styles.textinput4}/>
+                <TextInput placeholder={'Referral Code (Optional)'} style={styles.input5}/>       
+            </View>     
+            </View>
+            <View style={styles.Button}>
+                <Btn Button={() => this.props.navigation.navigate('Signup9')}  title={'Next'}  />
+            </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    inputtextcontainer:{
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop: h('3%')
+    },
+    input1:{
+        height:h('17%'),
+        width: w('85%'),
+        borderWidth: h('0.08%'),
+        borderRadius: h('2%'), 
+    },
+    seprator:{
+        height: h('2%'),
+    },
+    textinput4:{
+        height:h('8%'),
+        width:w('80%'),
+        borderWidth: h('0.08%'),
+        borderRadius: h('1.3%'),  
+        marginBottom: h('1%'),
+    },
+    input5:{
+        height:h('8%'),
+        width:w('80%'),
+        borderWidth: h('0.08%'),
+        borderRadius: h('1.3%'), 
+    },
+    Button:{
+        marginTop: h('2%'),
+        alignItems:'center',
+    }
+})
--- src/screen/Welcome.js
+++ src/screen/Welcome.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
 
 import {HeaderText} from '../component/HeaderText'
@@ -7,23 +7,22 @@ import {HeaderText} from '../component/HeaderText'
 export class Welcome extends React.Component{
     render(){
         return(
-            <View style={{
-                flex:1,
-                // backgroundColor:'green',
-            }}>
-                <HeaderText
-                titles={'WELCOME TO STAT BANKING'}
-                />
-                <View style={{
-                    height:h('85%'),
-                    width: w('100%'),
-                    // backgroundColor:'pink',
-                    alignItems:'center',
-                    justifyContent:'center',
-                }}>
+            <View style={styles.parent}>
+                <HeaderText titles={'WELCOME TO STAT BANKING'}/>
+            <View style={styles.maincontainer}>
                 <Text> Empty Screen </Text>
-                </View>
-            </View>
+            </View></View>
         )
     }
 }
+const styles = StyleSheet.create({
+    parent:{
+        flex:1,
+    },
+    maincontainer:{
+        height:h('85%'),
+        width: w('100%'),
+        alignItems:'center',
+        justifyContent:'center',
+    }
+})
