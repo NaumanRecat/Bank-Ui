79c293512c5d29c9674bb500db8fbb802360f83e
NaumanRecat <naumantariq2977@gmail.com>
2021-10-08 02:32:55 -0700

    Signup3

--- src/screen/Signup3.js
+++ src/screen/Signup3.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
 
 import {HeaderText} from '../component/HeaderText';
@@ -10,31 +10,29 @@ import { Btn } from "../component/Btn";
 export class Signup3 extends React.Component{
     render(){
         return(
-            <View style={{
-                flex:1,
-            }}>
-                <HeaderText 
-                titles={'StatBanking'}
-                />
-                <ContentText
-                title={'How can we Contact you?'}
-                />
-                <View style={{
-                    marginTop:h('12%'),
-                }}>
-                <Input placeholder={'+_(_ _ _)-_ _ _-_ _ _ _'} />
-                <Input placeholder={'Email'} />
-                </View>
-
-                <View style={{
-                    alignItems:'center',
-                    justifyContent:'center',
-                    marginTop:'40%'
-                }}>
-                <Btn Button={() => this.props.navigation.navigate('Signup4')} title={'Next'} />
-                </View>
-               
-            </View>
+            <View style={styles.parentView}>
+            <HeaderText titles={'StatBanking'}/>
+            <ContentText title={'How can we Contact you?'}/>
+            <View style={styles.parentmargin}>
+            <Input placeholder={'+ _ ( _  _  _ ) - _   _  _ - _  _  _  _'} />
+            <Input placeholder={'Email'} /></View>
+            <View style={styles.ButtonView}>
+            <Btn Button={() => this.props.navigation.navigate('Signup4')} title={'Next'} />
+            </View></View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    parentView:{
+        flex:1,
+    },
+    parentmargin:{
+        marginTop:h('12%'),
+    },
+    ButtonView:{
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop:'49%'
+    }
+})
